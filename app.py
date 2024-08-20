4026019ae742005d48af4b00d2463aed8f4fd933
ThomasCeccon <cecconthomas275@gmail.com>
2024-08-20 19:40:48 -0300

    Sincronizar e ajustar tela

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
 
+#importando as classes
+from flask import Flask,jsonify,render_template
+from flask import request
+
+#instancias a classe flask
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+#rotas de acesso
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
 
+#executado apenas no arquivo principal
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='0.0.0.0', port=8080) #executar o flask
+   
+
